6694187888f06d48fea3ef88fb801ac35e0c3821
hchiam <hchiam@gmail.com>
2020-03-04 17:58:42 -0500

    add .* files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'settings': {
+  },
+  'rules': {
+  },
+};
+
+/**
+ * This file was first created using the Yeoman generator
+ * generator-hchiam-learning:
+ * https://www.npmjs.com/package/generator-hchiam-learning
+ */
